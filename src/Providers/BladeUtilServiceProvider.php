3eb26333a2784ec45314395f32546033c9eeacfa
0nepeop1e <engshun.lim@gmail.com>
2019-05-03 13:48:21 +0800

    cleanup code

--- src/Providers/BladeUtilServiceProvider.php
+++ src/Providers/BladeUtilServiceProvider.php
@@ -12,7 +12,7 @@ use Illuminate\Support\ServiceProvider;
 class BladeUtilServiceProvider extends ServiceProvider
 {
     public function boot(){
-        $this->app->bind(BladeUtilsImpl::class, function(){
+        $this->app->singleton(BladeUtilsImpl::class, function(){
             return new BladeUtilsImpl();
         });
         $this->app->alias(BladeUtilsImpl::class, 'blade-utils');
@@ -35,113 +35,19 @@ class BladeUtilServiceProvider extends ServiceProvider
             return $result;
         });
 
-        Blade::directive('json', function($expression){
-            $args = BladeUtils::parseArguments($expression)->toArray();
-            if(count($args) < 2){
-                $args[]= '15';
-            }
-            if(count($args) < 3){
-                $args[]= '512';
-            }
-            return "<?php echo json_encode($args[0], $args[1], $args[2]); ?>";
-        });
+        Blade::directive('json', BladeUtils::class.'::compileJson');
 
         Blade::directive('each', function ($expression){
             return '<?php echo $__env->renderEachWithVars(get_defined_vars(), '.$expression.') ?>';
         });
 
-        Blade::directive('iif', function ($expression){
-            $args = BladeUtils::parseArguments($expression);
-            if($args->count() == 2 && $args[1]->isSimple()){
-                return "<?php if($args[0]): ?>".e($args[1]->val())."<?php endif; ?>";
-            }
-            if($args->count() == 3 && $args[1]->isSimple() && $args[2]->isSimple()){
-                return "<?php if($args[0]): ?>".e($args[1]->val())."<?php else: ?>".e($args[2]->val())."<?php endif; ?>";
-            }
-            $arg3 = $args->count() >= 3 ? $args[2] : "''";
-            return "<?php echo e(($args[0]) ? ($args[1]) : ($arg3)); ?>";
-        });
+        Blade::directive('json', BladeUtils::class.'::compileIif');
 
-        Blade::directive('css', function ($expression) {
-            $args = BladeUtils::parseArguments($expression);
-            if($args[0]->isSimple()){
-                $href = e($args[0]->val());
-            } else {
-                $href = "<?php echo e($args[0]); ?>";
-            }
-            $link = "<link rel=\"stylesheet\" href=\"$href\"";
-            if($args->count() > 1){
-                if($args[1]->isSimple()){
-                    $attr = e($args[1]->val());
-                } else {
-                    $attr = "<?php echo e($args[1]); ?>";
-                }
-                $link .= " integrity=\"$attr\"";
-            }
-            if($args->count() > 2){
-                if($args[2]->isSimple()){
-                    $attr = e($args[2]->val());
-                } else {
-                    $attr = "<?php echo e($args[2]); ?>";
-                }
-                $link .= " crossorigin=\"$attr\"";
-            }
-            return $link.'/>';
-        });
+        Blade::directive('css', BladeUtils::class.'::compileCss');
 
-        Blade::directive('js', function ($expression) {
-            $args = BladeUtils::parseArguments($expression);
-            if($args[0]->isSimple()){
-                $src = e($args[0]->val());
-            } else {
-                $src = "<?php echo e($args[0]); ?>";
-            }
-            $script = "<script type=\"text/javascript\" language=\"js\" src=\"$src\"";
-            if($args->count() > 1){
-                if($args[1]->isSimple()){
-                    $attr = e($args[1]->val());
-                } else {
-                    $attr = "<?php echo e($args[1]); ?>";
-                }
-                $script .= " integrity=\"$attr\"";
-            }
-            if($args->count() > 2){
-                if($args[2]->isSimple()){
-                    $attr = e($args[2]->val());
-                } else {
-                    $attr = "<?php echo e($args[2]); ?>";
-                }
-                $script .= " crossorigin=\"$attr\"";
-            }
-            return $script.'</script>';
-        });
+        Blade::directive('js', BladeUtils::class.'::compileJs');
 
-        Blade::directive('img', function ($expression) {
-            $args = BladeUtils::parseArguments($expression);
-            if($args[0]->isSimple()){
-                $src = e($args[0]->val());
-            } else {
-                $src = "<?php echo e($args[0]); ?>";
-            }
-            $script = "<img src=\"$src\"";
-            if($args->count() > 1){
-                if($args[1]->isSimple()){
-                    $attr = e($args[1]->val());
-                } else {
-                    $attr = "<?php echo e($args[1]); ?>";
-                }
-                $script .= " alt=\"$attr\"";
-            }
-            if($args->count() > 2){
-                if($args[2]->isSimple()){
-                    $attr = e($args[2]->val());
-                } else {
-                    $attr = "<?php echo e($args[2]); ?>";
-                }
-                $script .= " class=\"$attr\"";
-            }
-            return $script.'/>';
-        });
+        Blade::directive('img', BladeUtils::class.'::compileImg');
 
         Blade::directive('nameMeta', function ($expression){
             return "<?php echo \\".BladeUtils::class."::buildNameMeta($expression); ?>";
--- src/Tools/BladeUtils.php
+++ src/Tools/BladeUtils.php
@@ -3,11 +3,114 @@
 
 namespace Eslym\BladeUtils\Tools;
 
-
 class BladeUtils
 {
     public function parseArguments(string $expressions): Arguments{
         return new Arguments($expressions);
+    }
+
+    public function compileJson($expression): string{
+        $args = $this->parseArguments($expression)->toArray();
+        if(count($args) < 2){
+            $args[]= '15';
+        }
+        if(count($args) < 3){
+            $args[]= '512';
+        }
+        return "<?php echo json_encode($args[0], $args[1], $args[2]); ?>";
+    }
+
+    public function compileIif($expression): string{
+        $args = $this->parseArguments($expression);
+        if($args->count() == 2 && $args[1]->isSimple()){
+            return "<?php if($args[0]): ?>".e($args[1]->val())."<?php endif; ?>";
+        }
+        if($args->count() == 3 && $args[1]->isSimple() && $args[2]->isSimple()){
+            return "<?php if($args[0]): ?>".e($args[1]->val())."<?php else: ?>".e($args[2]->val())."<?php endif; ?>";
+        }
+        $arg3 = $args->count() >= 3 ? $args[2] : "''";
+        return "<?php echo e(($args[0]) ? ($args[1]) : ($arg3)); ?>";
+    }
+
+    public function compileCss($expression): string{
+        $args = $this->parseArguments($expression);
+        if($args[0]->isSimple()){
+            $href = e($args[0]->val());
+        } else {
+            $href = "<?php echo e($args[0]); ?>";
+        }
+        $link = "<link rel=\"stylesheet\" href=\"$href\"";
+        if($args->count() > 1){
+            if($args[1]->isSimple()){
+                $attr = e($args[1]->val());
+            } else {
+                $attr = "<?php echo e($args[1]); ?>";
+            }
+            $link .= " integrity=\"$attr\"";
+        }
+        if($args->count() > 2){
+            if($args[2]->isSimple()){
+                $attr = e($args[2]->val());
+            } else {
+                $attr = "<?php echo e($args[2]); ?>";
+            }
+            $link .= " crossorigin=\"$attr\"";
+        }
+        return $link.'/>';
+    }
+
+    public function compileJs($expression):string{
+        $args = $this->parseArguments($expression);
+        if($args[0]->isSimple()){
+            $src = e($args[0]->val());
+        } else {
+            $src = "<?php echo e($args[0]); ?>";
+        }
+        $script = "<script type=\"text/javascript\" language=\"js\" src=\"$src\"";
+        if($args->count() > 1){
+            if($args[1]->isSimple()){
+                $attr = e($args[1]->val());
+            } else {
+                $attr = "<?php echo e($args[1]); ?>";
+            }
+            $script .= " integrity=\"$attr\"";
+        }
+        if($args->count() > 2){
+            if($args[2]->isSimple()){
+                $attr = e($args[2]->val());
+            } else {
+                $attr = "<?php echo e($args[2]); ?>";
+            }
+            $script .= " crossorigin=\"$attr\"";
+        }
+        return $script.'</script>';
+    }
+
+    public function compileImg($expression) {
+        $args = $this->parseArguments($expression);
+        if($args[0]->isSimple()){
+            $src = e($args[0]->val());
+        } else {
+            $src = "<?php echo e($args[0]); ?>";
+        }
+        $script = "<img src=\"$src\"";
+        if($args->count() > 1){
+            if($args[1]->isSimple()){
+                $attr = e($args[1]->val());
+            } else {
+                $attr = "<?php echo e($args[1]); ?>";
+            }
+            $script .= " alt=\"$attr\"";
+        }
+        if($args->count() > 2){
+            if($args[2]->isSimple()){
+                $attr = e($args[2]->val());
+            } else {
+                $attr = "<?php echo e($args[2]); ?>";
+            }
+            $script .= " class=\"$attr\"";
+        }
+        return $script.'/>';
     }
 
     public function buildPropMeta(array $meta): string{
