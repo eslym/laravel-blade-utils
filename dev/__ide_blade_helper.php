a0803c6cd9c81ccf044e45f97e8821c15256b44c
0nepeop1e <engshun.lim@gmail.com>
2019-06-11 13:45:12 +0800

    fix type hint

--- dev/__ide_blade_helper.php
+++ dev/__ide_blade_helper.php
@@ -5,7 +5,7 @@
  * @method static void js($url, $sri = null, $crossOrigin = null)
  * @method static void css($url, $sri = null, $crossOrigin = null)
  * @method static void iif($statement, $ifTrue, $ifFalse = null)
- * @method static void img($url, $class = null)
+ * @method static void img($url, $alt = null, $class = null)
  * @method static void meta(string|string[] $attributes, string[] $content)
  * @method static void nameMeta(string[] $content)
  * @method static void itemMeta(string[] $content)
